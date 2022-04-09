602e8f0006dd900b98f7218275156b647ef75d04
Pareeesa <parisa.faridi80@gmail.com>
2022-04-10 00:25:22 +0430

    Add correct answer for each question

--- app/src/main/java/com/example/livedatatest/Question.kt
+++ app/src/main/java/com/example/livedatatest/Question.kt
@@ -1,3 +1,3 @@
 package com.example.livedatatest
 
-class Question(var text:String)
+class Question(var text:String,var correctAnswer:Int)
--- app/src/main/java/com/example/livedatatest/QuestionRepository.kt
+++ app/src/main/java/com/example/livedatatest/QuestionRepository.kt
@@ -3,9 +3,9 @@ package com.example.livedatatest
 object QuestionRepository {
 
     val questionList = arrayListOf(
-        Question("2+2"), Question("5-2"),
-        Question("8+10"), Question("3*2"), Question("7-6"),
-        Question("1+8"), Question("5-6"), Question("10*10"),
-        Question("9-6"), Question("4*8")
+        Question("2+2",4), Question("5-2",3),
+        Question("8+10",18), Question("3*2",6), Question("7-6",1),
+        Question("1+8",9), Question("5-6",-1), Question("10*10",100),
+        Question("9-6",3), Question("4*8",32)
     )
 }
