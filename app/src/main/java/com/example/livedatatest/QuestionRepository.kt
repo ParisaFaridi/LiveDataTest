1be9eaf483840705a274469615c7c5ae6ea03e23
Pareeesa <parisa.faridi80@gmail.com>
2022-04-14 11:00:59 +0430

    Use Dao's queries in repository

--- app/src/main/java/com/example/livedatatest/QuestionRepository.kt
+++ app/src/main/java/com/example/livedatatest/QuestionRepository.kt
@@ -1,11 +1,35 @@
 package com.example.livedatatest
 
-object QuestionRepository {
+import androidx.lifecycle.LiveData
 
-    val questionList = arrayListOf(
-        Question("2+2",4), Question("5-2",3),
-        Question("8+10",18), Question("3*2",6), Question("7-6",1),
-        Question("1+8",9), Question("5-6",-1), Question("10*10",100),
-        Question("9-6",3), Question("4*8",32)
-    )
+class QuestionRepository(private val questionDao: QuestionDao) {
+
+    val questionList = questionDao.getAll()
+
+    //    fun addQuestion() {
+//        questionDao.addQuestion(
+//            Question(0, "2+2", 4), Question(0, "5-2", 3),
+//            Question(0, "8+10", 18), Question(0, "3*2", 6), Question(0, "7-6", 1),
+//            Question(0, "1+8", 9), Question(0, "5-6", -1), Question(0, "10*10", 100),
+//            Question(0, "9-6", 3), Question(0, "4*8", 32)
+//        )
+//    }
+    fun addTestData() {
+        for (i in 0..9)
+            newRandomQuestion()
+    }
+
+    fun newRandomQuestion() {
+        val a = (1..100).random()
+        val b = (1..50).random()
+        questionDao.addQuestion(Question(0, "$a - $b", a - b))
+    }
+
+    fun getCount(): LiveData<Int> {
+        return questionDao.getCount()
+    }
+
+    fun getQuesitonById(id: Int): Question {
+        return questionDao.getQuestionById(id)
+    }
 }
